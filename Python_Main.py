3b7fa966401804f179d1dddc9222a608b82a2d2d
francescofraternali <francesco.fraternali@gmail.com>
2019-02-21 17:59:36 -0800

    Update Python_Main.py

--- Python_Main.py
+++ Python_Main.py
@@ -96,12 +96,12 @@ print "Let us Start!!"
 while(1):
 	for x in range(0,len(Sens_Splitted)):
 		
-		subprocess.Popen("bash Find_New_BLE_Device.sh > dev_found.txt", shell=True)
-		sleep(2)
-		with open("dev_found.txt", 'r') as f:
-			data = f.read()
-		for i in range(len(data)):
-			print(data[i])
+		#subprocess.Popen("bash Find_New_BLE_Device.sh > dev_found.txt", shell=True)
+		#sleep(2)
+		#with open("dev_found.txt", 'r') as f:
+		#	data = f.read()
+		#for i in range(len(data)):
+		#	print(data[i])
 		
 		ID = ID_List[x]
 		Name = str(Name_List[x])
