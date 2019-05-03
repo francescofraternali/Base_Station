cc63f3bfc28b2afa093704fb2873e136a15fd8f0
francescofraternali <francesco.fraternali@gmail.com>
2019-05-03 14:20:03 -0700

    Update multipy_main.py

--- multipy_main.py
+++ multipy_main.py
@@ -243,9 +243,9 @@ while(True):
 
     sleep(1)
 
-    subprocess.Popen("cat /var/log/auth.log | grep 'Accepted password' > Accepted_file.txt", shell=True)
-    subprocess.Popen('tail -1 Accepted_file.txt > Accepted_file.txt', shell=True)
-    with open('Accepted_file.txt', 'r') as f:
+    #subprocess.Popen("cat /var/log/auth.log | grep 'Accepted password' > Accepted_file.txt", shell=True)
+    #subprocess.Popen('tail -1 Accepted_file.txt > Accepted_file.txt', shell=True)
+    with open('/var/log/auth.log', 'r') as f:
         for line in f:
             print(line)
 	    break
