05596806e0cc1b7045c23ba8922a1020749f2ac3
francescofraternali <francesco.fraternali@gmail.com>
2019-05-03 12:59:28 -0700

    Update multipy_main.py

--- multipy_main.py
+++ multipy_main.py
@@ -246,6 +246,7 @@ while(True):
     subprocess.Popen("cat /var/log/auth.log | grep 'Accepted password' > Accepted_file.txt", shell=True)
     out = subprocess.Popen('tail -1 Accepted_file.txt', stdout=subprocess.PIPE, shell=True)
     #out = out.decode()
+    print(out)
     spl = out.strip().split('')
     clock = spl[2].split(':')
 
