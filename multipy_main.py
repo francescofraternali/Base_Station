9222a4e2d6d9aca353f65bf95495a251e1d35afb
francescofraternali <francesco.fraternali@gmail.com>
2019-05-03 15:45:46 -0700

    Update multipy_main.py

--- multipy_main.py
+++ multipy_main.py
@@ -257,20 +257,23 @@ while(True):
     clock = spl[3].split(':')
 	
     now = datetime.datetime.now()
-    
+    now_time = now.strftime('%m/%d/%y %H:%M:%S')
     
     #print(clock[0])
     month = datetime.datetime.strptime(spl[0], '%b')
     last_time = datetime.datetime(int(now.year), int(month.month), int(spl[2]), int(clock[0]), int(clock[1]), int(clock[2]))
-    with open('last_time.txt', 'r') as f:
-        out = f.readlines()
-    now_last = datetime.datetime.strptime(out, '%m/%d/%y %H:%M:%S')
-    print(now_last)
-
+    try:
+        with open('last_time.txt', 'r') as f:
+            out = f.readlines()
+        now_last = datetime.datetime.strptime(out, '%m/%d/%y %H:%M:%S')
+        print(now_last)
+    except:
+	with open('last_time.txt', 'w') as f:
+	    f.write(now_time)
+	
     diff_1 = (now - last_time).total_seconds()
     diff_2 = (now - now_last).total_seconds()
     if diff_1 > 60*1 and diff_2 > 60*1:
-	now_time = now.strftime('%m/%d/%y %H:%M:%S')
 	with open('last_time.txt', 'w') as f:
 	    f.write(now_time)
 	sleep(1)
